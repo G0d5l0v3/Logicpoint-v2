7ee0cefce28641bd740af604ab865fbe517a89a0
Godslove Udo <udogodslove@gmail.com>
2024-01-21 23:39:27 +0100

    update

--- src/pages/home/sections/WhatMotivatesUs.jsx
+++ src/pages/home/sections/WhatMotivatesUs.jsx
@@ -1,19 +1,13 @@
 const WhatMotivatesUs = () => {
   return (
     <div className="bg-[#181830]">
-      <div
-        className="font-[exo] text-white pb-[5rem] flex flex-col md:flex max-w-[1440px] mx-auto px-[1rem] sm:px-[1rem]"
-        data-aos="fade-left"
-        data-aos-duration="1000"
-      >
+      <div className="font-[exo] text-white pb-[5rem] flex flex-col md:flex max-w-[1440px] mx-auto px-[1rem] sm:px-[1rem]">
         <h1 className="font-semibold text-3xl sm:text-4xl lg:text-5xl py-[0.3rem]">
           <span className="border-b-8 border-[#5D1570] ">What </span>
           <span className="text-[#E87B37]">Motivates </span>
           <span>Us </span>{" "}
         </h1>
-        <div
-          className="text-white font-[exo]"
-        >
+        <div className="text-white font-[exo]">
           <p className="pt-[2rem] pb-[0.3rem}">
             There are no limitations in terms of the industries we can assist,
             as we employ and upskill our teams{" "}
