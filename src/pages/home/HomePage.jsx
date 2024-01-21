ca849c79c5404c413e2ea962c5b7d635b7b19f9e
Godslove Udo <udogodslove@gmail.com>
2024-01-21 22:59:45 +0100

    update OurCommitment.jsx

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Hero from "./sections/Hero";
 import WhatWeDo from "./sections/WhatWeDo";
 import ServicesWeOffer from "./sections/ServicesWeOffer";
+import OurCommitment from "./sections/OurCommitment";
 
 const HomePage = () => {
   return (
@@ -9,6 +10,7 @@ const HomePage = () => {
       <Hero />
       <WhatWeDo />
       <ServicesWeOffer />
+      <OurCommitment />
     </div>
   );
 };
---
+++ src/pages/home/sections/OurCommitment.jsx
@@ -0,0 +1,101 @@
+import Button from "../../../components/button/Button";
+
+const OurCommitment = () => {
+  return (
+    <div className="bg-[#181830]">
+      <div className="font-[exo] min-h-[70svh] flex flex-col md:flex justify-center pt-[2rem] max-w-[1440px] mx-auto px-[2rem] sm:px-[5rem]">
+        <div>
+          <h1
+            className="text-white py-[2rem] font-semibold text-2xl sm:text-4xl lg:text-5xl py-[0.3rem] text-[#181830]"
+            data-aos="fade-right"
+            data-aos-duration="1000"
+          >
+            <span className="border-b-8 border-[#5D1570] ">Our </span>
+            <span>Commitment </span>
+            <span className="text-[#E87B37]">to You</span>{" "}
+          </h1>
+        </div>
+        <div
+          className="text-white font-[Exo]"
+          data-aos="fade-zoom-in"
+          data-aos-easing="ease-in-out"
+          data-aos-delay="500"
+          data-aos-offset="0"
+        >
+          <div className="grid pt-[2rem] md:flex gap-3">
+            <div>
+              <h1 className="font-bold text-[#E87B37]">Exponential Growth</h1>
+              <div className="pt-[1rem]">
+                <div className="flex items-center gap-3 py-[1rem]">
+                  <span className="flex items-center justify-center rounded-full h-10 w-10 bg-[#E87B37]">
+                    1
+                  </span>{" "}
+                  <p className="max-w-[80%] text-sm sm:text-md lg:text-lg">
+                    Committed 24/7 to generating sales and new customers for
+                    your business.
+                  </p>
+                </div>
+                <div className="flex items-center gap-3 py-[1rem]">
+                  <span className="flex items-center justify-center rounded-full h-10 w-10 bg-[#E87B37]">
+                    2{" "}
+                  </span>{" "}
+                  <p className="max-w-[80%] text-sm sm:text-md lg:text-lg">
+                    Proficient, highly skilled agents providing world-class
+                    customer services.
+                  </p>
+                </div>
+                <div className="flex items-center gap-3 py-[1rem]">
+                  <span className="flex items-center justify-center rounded-full h-10 w-10 bg-[#E87B37]">
+                    3{" "}
+                  </span>{" "}
+                  <p className="max-w-[80%] text-sm sm:text-md lg:text-lg">
+                    Multi-process outsourced solutions that enable your business
+                    to scale.
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div>
+              <h1 className="font-bold text-[#E87B37]">Cost Efficiency</h1>
+              <div className="grid pt-[1rem]">
+                <div className="flex items-center gap-3 py-[1rem]">
+                  <span className="flex items-center justify-center rounded-full h-10 w-10 bg-[#E87B37]">
+                    1{" "}
+                  </span>{" "}
+                  <p className="max-w-[80%] text-sm sm:text-md lg:text-lg">
+                    Cost reductions of up to 70% for frontline and back office
+                    services.
+                  </p>
+                </div>
+                <div className="flex items-center gap-3 py-[1rem]">
+                  <span className="flex items-center justify-center rounded-full h-10 w-10 bg-[#E87B37]">
+                    2{" "}
+                  </span>{" "}
+                  <p className="max-w-[80%] text-sm sm:text-md lg:text-lg">
+                    Multi-talented teams of customer service and support from
+                    seasoned experts.
+                  </p>
+                </div>
+                <div className="flex items-center gap-3 py-[1rem]">
+                  <span className="flex items-center justify-center rounded-full h-10 w-10 bg-[#E87B37]">
+                    3{" "}
+                  </span>{" "}
+                  <p className="max-w-[80%] text-sm sm:text-md lg:text-lg">
+                    Multi-process outsourced solutions that enable your business
+                    to scale.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="my-[3rem] hover:opacity-[0.7]">
+            <Button description="Talk to Us" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default OurCommitment;
