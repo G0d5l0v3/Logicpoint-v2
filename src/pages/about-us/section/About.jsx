87b81f9386d5ffd7b927b934f986a04273d3724f
Godslove Udo <udogodslove@gmail.com>
2024-01-24 14:07:20 +0100

    Update About.jsx

--- src/pages/about-us/section/About.jsx
+++ src/pages/about-us/section/About.jsx
@@ -15,10 +15,10 @@ const About = () => {
         <h1 className="font-[exo] text-[#181830] text-4xl sm:text-5xl lg:whitespace-normal lg:text-6xl font-semibold leading-tight">
           <span>
             Committed to{" "}
-            <span className="text-[#E87B37]">elevating your business </span>{" "}
+            <span className="text-[#5D1570]">elevating your business </span>{" "}
           </span>
           <br />
-          <span> through step-by-step collaborations</span>
+          <span> through <span className="text-[#E87B37]">step-by-step </span> collaborations</span>
         </h1>
 
         <p className="py-[1rem]  lg:whitespace-normal sm:py-[2rem] font-[poppins] text-[#181830] text-sm sm:text-md lg:text-lg leading-tight">
