1a27f403b1ac8f4585b4cf7ec2dc057c854776a4
Godslove Udo <udogodslove@gmail.com>
2024-01-24 06:55:33 +0100

    Update About.jsx

--- src/pages/about-us/section/About.jsx
+++ src/pages/about-us/section/About.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ImageComponent from "../../../components/imageComponent/ImageComponent"
+import ImageComponent from "../../../components/imageComponent/ImageComponent";
 
 const About = () => {
   return (
@@ -7,15 +7,12 @@ const About = () => {
       <div className="px-[1rem] py-[5rem] lg:max-w-[1240px] lg:mx-[auto] ">
         <h1 className="font-[exo] text-[#181830] text-3xl sm:text-4xl lg:whitespace-normal lg:text-5xl font-semibold leading-tight">
           <span>
-          Committed to  {" "}
+            Committed to {" "}
             <span className="text-[#E87B37]">elevating your business </span>{" "}
-            through{" "}
           </span>
           <br />
-          <span>step-by-step collaborations</span>
+          <span> through step-by-step collaborations</span>
         </h1>
-     
-        
       </div>
     </div>
   );
