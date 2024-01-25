3d6e097f9df99d84d79ee2c83cf4def095cffc1f
Godslove Udo <udogodslove@gmail.com>
2024-01-25 13:38:27 +0100

    Update AboutWhatWeDo.jsx

--- src/pages/about-us/section/AboutWhatWeDo.jsx
+++ src/pages/about-us/section/AboutWhatWeDo.jsx
@@ -2,15 +2,45 @@ import React from "react";
 
 const AboutWhatWeDo = () => {
   return (
-    <div className="bg-[#181830] px-[1rem] py-[5rem] ">
+    <div className="bg-[#181830] px-[1rem] py-[5rem] text-white">
       <div className="flex items-center justify-between lg:max-w-[1240px] lg:mx-[auto]">
         <div>
-          <h1 className="font-[exo] font-semibold text-3xl sm:text-4xl lg:text-5xl text-white">
+          <h1 className="font-[exo] font-semibold text-3xl sm:text-4xl lg:text-5xl ">
             <span className="text-[#E87B37] border-b-8 border-[#5D1570] ">
               Who
             </span>{" "}
             <span>We Are</span>
           </h1>
+          <p className="pt-[3rem] lg:whitespace-normal font-[poppins] text-sm sm:text-md lg:text-lg leading-tight">
+            <span>There are no limitations in terms of the industries </span>
+            <br className="hidden sm:flex" />
+            <span>we can assist, as we employ and upskill our teams </span>
+            <br className="hidden sm:flex" />
+            <span>based on the customized requirements of our </span>
+            <br className="hidden sm:flex" />
+            <span>partner clients.</span>
+          </p>
+          <p className="pt-[1rem] lg:whitespace-normal font-[poppins] text-sm sm:text-md lg:text-lg leading-tight">
+            <span>There are no limitations in terms of the industries </span>
+            <br className="hidden sm:flex" />
+            <span>we can assist, as we employ and upskill our teams </span>
+            <br className="hidden sm:flex" />
+            <span>based on the customized requirements of our </span>
+            <br className="hidden sm:flex" />
+            <span>partner clients.</span>
+          </p>
+          <p className="pt-[1rem] lg:whitespace-normal font-[poppins] text-sm sm:text-md lg:text-lg leading-tight">
+            <span>There are no limitations in terms of the industries </span>
+            <br className="hidden sm:flex" />
+            <span>we can assist, as we employ and upskill our teams </span>
+            <br className="hidden sm:flex" />
+            <span>based on the customized requirements of our </span>
+            <br className="hidden sm:flex" />
+            <span>partner clients.</span>
+          </p>
+        </div>
+        <div>
+            
         </div>
       </div>
     </div>
