cc3192253de53877ae95af7fc39aedce741d1200
Godslove Udo <udogodslove@gmail.com>
2024-01-25 13:19:50 +0100

    update About.jsx

--- src/pages/about-us/AboutPage.jsx
+++ src/pages/about-us/AboutPage.jsx
@@ -1,9 +1,11 @@
 import About from "./section/About"
+import AboutWhatWeDo from "./section/AboutWhatWeDo"
 
 const AboutPage = () => {
   return (
     <div className="">
       <About />
+      <AboutWhatWeDo />
     </div>
   )
 }
---
+++ src/pages/about-us/section/AboutWhatWeDo.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const AboutWhatWeDo = () => {
+  return (
+    <div className="bg-[#181830] px-[1rem] py-[5rem] ">
+      <div className="flex items-center justify-between lg:max-w-[1240px] lg:mx-[auto]">
+        <div>
+          <h1 className="font-[exo] font-semibold text-3xl sm:text-4xl lg:text-5xl text-white">
+            <span className="text-[#E87B37] border-b-8 border-[#5D1570] ">
+              Who
+            </span>{" "}
+            <span>We Are</span>
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AboutWhatWeDo;
