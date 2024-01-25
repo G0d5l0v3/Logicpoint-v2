63216981adb79ab7741b073d251e04e4b090a193
Godslove Udo <udogodslove@gmail.com>
2024-01-25 11:56:31 +0100

    Update About.jsx

--- src/pages/about-us/section/About.jsx
+++ src/pages/about-us/section/About.jsx
@@ -3,10 +3,10 @@ import ImageComponent from "../../../components/imageComponent/ImageComponent";
 
 const About = () => {
   const image = {
-    path: "https://res.cloudinary.com/db5az7ydi/image/upload/f_auto,q_auto/v1/logicpoint-assets/qhe2t8i66zndto2i5apz",
+    path: "https://res.cloudinary.com/db5az7ydi/image/upload/f_auto,q_auto/v1/logicpoint-assets/sqsc3zaraxsr9lhvigly",
     alt: "what-we-do-poster",
     style: "h-80 2xl:h-100 w-full object-cover rounded-lg",
-    hash: "LLJkTA.8tn?v?^%1of-;9EIUMwoz",
+    hash: "LAQbIX%f{1M|e.aeysxFqvRjbtoz",
   };
 
   return (
