cd6ce6bd5eca389650d34abb2ad344eb28ba2916
Godslove Udo <udogodslove@gmail.com>
2024-01-27 23:51:30 +0100

    Update AboutWhatWeDo.jsx

--- src/pages/about-us/section/AboutWhatWeDo.jsx
+++ src/pages/about-us/section/AboutWhatWeDo.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const AboutWhatWeDo = () => {
   return (
     <div className="bg-[#181830] px-[1rem] py-[5rem] text-white font-[exo]">
-      <div className="grid md:flex items-center justify-between lg:max-w-[1240px] lg:mx-[auto]">
+      <div className="grid md:flex items-center text-center justify-center lg:max-w-[1240px] lg:mx-[auto]">
         <div>
           <h1 className="font-semibold text-3xl sm:text-4xl lg:text-5xl ">
             <span className="text-[#E87B37] border-b-8 border-[#5D1570] ">
@@ -20,47 +20,8 @@ const AboutWhatWeDo = () => {
             <br className="hidden sm:flex" />
             <span>partner clients.</span>
           </p>
-          <p className="pt-[1rem] lg:whitespace-normal font-[poppins] text-sm sm:text-md lg:text-lg leading-tight">
-            <span>There are no limitations in terms of the industries </span>
-            <br className="hidden sm:flex" />
-            <span>we can assist, as we employ and upskill our teams </span>
-            <br className="hidden sm:flex" />
-            <span>based on the customized requirements of our </span>
-            <br className="hidden sm:flex" />
-            <span>partner clients.</span>
-          </p>
-          <p className="pt-[1rem] lg:whitespace-normal font-[poppins] text-sm sm:text-md lg:text-lg leading-tight">
-            <span>There are no limitations in terms of the industries </span>
-            <br className="hidden sm:flex" />
-            <span>we can assist, as we employ and upskill our teams </span>
-            <br className="hidden sm:flex" />
-            <span>based on the customized requirements of our </span>
-            <br className="hidden sm:flex" />
-            <span>partner clients.</span>
-          </p>
         </div>
-        <div className="flex flex-col gap-16 text-[#E87B37] w-[40%] font-semibold text-3xl sm:text-4xl lg:text-5xl ">
-          <div className="flex items-center justify-around">
-            <div className="flex flex-col">
-              <p>50%</p>
-              <p className="text-white text-sm sm:text-md lg:text-lg">Female Employees</p>
-            </div>
-            <div>
-              <p>90%</p>
-              <p className="text-white text-sm sm:text-md lg:text-lg">Graduates</p>
-            </div>
-          </div>
-          <div className="flex items-center justify-around">
-            <div className="flex flex-col ">
-              <p>100%</p>
-              <p className="text-white text-sm sm:text-md lg:text-lg">Technical Professionals</p>
-            </div>
-            <div>
-              <p>50+</p>
-              <p className="text-white text-sm sm:text-md lg:text-lg">Employees</p>
-            </div>
-          </div>
-        </div>
+
       </div>
     </div>
   );
