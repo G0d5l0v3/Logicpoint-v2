4183e66774b0c776dd0acb1bc8d25892bee60e8b
Godslove Udo <udogodslove@gmail.com>
2024-01-27 11:26:05 +0100

    update AboutWhatWeDo.jsx

--- src/components/items/OurServicesBox.jsx
+++ src/components/items/OurServicesBox.jsx
@@ -3,7 +3,7 @@ const OurServicesBox = ({ services }) => {
     <div className="grid lg:grid-cols-4 gap-4 font-[exo]">
       {services.map((service) => (
         <div
-          className="grid bg-[#0a0a1a] text-white shadow-lg rounded-lg h-[20rem] 2xl:h-[25rem] px-[2rem] pt-[2rem]"
+          className="grid bg-[#0a0a1a] text-white shadow-lg rounded-lg h-[20rem] 2xl:h-[25rem] px-[1rem] sm:px-[2rem] pt-[2rem]"
           key={service.id}
         >
           <span className="flex items-center justify-center rounded-full h-10 w-10 bg-[#5D1570]">
--- src/pages/about-us/section/AboutWhatWeDo.jsx
+++ src/pages/about-us/section/AboutWhatWeDo.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const AboutWhatWeDo = () => {
   return (
-    <div className="bg-[#181830] px-[1rem] py-[5rem] text-white">
-      <div className="flex items-center justify-between lg:max-w-[1240px] lg:mx-[auto]">
+    <div className="bg-[#181830] px-[1rem] py-[5rem] text-white font-[exo]">
+      <div className="grid md:flex items-center justify-between lg:max-w-[1240px] lg:mx-[auto]">
         <div>
-          <h1 className="font-[exo] font-semibold text-3xl sm:text-4xl lg:text-5xl ">
+          <h1 className="font-semibold text-3xl sm:text-4xl lg:text-5xl ">
             <span className="text-[#E87B37] border-b-8 border-[#5D1570] ">
               Who
             </span>{" "}
@@ -39,8 +39,27 @@ const AboutWhatWeDo = () => {
             <span>partner clients.</span>
           </p>
         </div>
-        <div>
-            
+        <div className="flex flex-col gap-16 text-[#E87B37] w-[40%] font-semibold text-3xl sm:text-4xl lg:text-5xl ">
+          <div className="flex items-center justify-around">
+            <div className="flex flex-col">
+              <p>50%</p>
+              <p className="text-white text-sm sm:text-md lg:text-lg">Female Employees</p>
+            </div>
+            <div>
+              <p>90%</p>
+              <p className="text-white text-sm sm:text-md lg:text-lg">Graduates</p>
+            </div>
+          </div>
+          <div className="flex items-center justify-around">
+            <div className="flex flex-col ">
+              <p>100%</p>
+              <p className="text-white text-sm sm:text-md lg:text-lg">Technical Professionals</p>
+            </div>
+            <div>
+              <p>50+</p>
+              <p className="text-white text-sm sm:text-md lg:text-lg">Employees</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
