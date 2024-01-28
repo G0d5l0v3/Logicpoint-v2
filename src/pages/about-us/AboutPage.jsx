8bdb12209c7f98530407e1ad8d5c72291c0a65c4
Godslove Udo <udogodslove@gmail.com>
2024-01-28 16:08:45 +0100

    update AboutPage.jsx

--- src/pages/about-us/AboutPage.jsx
+++ src/pages/about-us/AboutPage.jsx
@@ -11,8 +11,7 @@ const AboutPage = () => {
     <>
       <About />
       <AboutWhatWeDo/>
-      <WhatWeOffer />
- 
+      <WhatWeOffer /> 
     </>
   )
 }
--- src/pages/about-us/section/AboutWhatWeDo.jsx
+++ src/pages/about-us/section/AboutWhatWeDo.jsx
@@ -1,37 +1,30 @@
-
 const AboutWhatWeDo = () => {
   return (
- 
-      <div className="bg-[#181830] px-[1rem] py-[5rem] text-white font-[exo]">
-        <div className="grid md:flex items-center text-center justify-center lg:max-w-[1240px] lg:mx-[auto]">
-          <div>
-            <h1 className="font-semibold text-3xl sm:text-4xl lg:text-5xl ">
-              <span className="text-[#E87B37] border-b-8 border-[#5D1570] ">
-                Who
-              </span>{" "}
-              <span>We Are</span>
-            </h1>
-            <p className="pt-[3rem] lg:whitespace-normal font-[poppins] text-sm sm:text-md lg:text-lg leading-tight">
-              There are no limitations in terms of the industries we can assist,
-              as we employ and upskill our teams based on the customized
-              requirements of our partner clients. There are no limitations in
-              terms of the industries we can assist, as we employ and upskill
-              our teams based on the customized requirements of our partner
-              clients. There are no limitations in terms of the industries we
-              can assist, as we employ and upskill our teams based on the
-              customized requirements of our partner clients. There are no
-              limitations in terms of the industries we can assist, as we employ
-              and upskill our teams based on the customized requirements of our
-              partner clients.
-            </p>
-          </div>
+    <div className="bg-[#181830] px-[1rem] py-[5rem] text-white font-[exo]">
+      <div className="grid md:flex items-center text-center justify-center lg:max-w-[1240px] lg:mx-[auto]">
+        <div>
+          <h1 className="font-semibold text-3xl sm:text-4xl lg:text-5xl ">
+            <span className="text-[#E87B37] border-b-8 border-[#5D1570] ">
+              Who
+            </span>{" "}
+            <span>We Are</span>
+          </h1>
+          <p className="pt-[3rem] lg:whitespace-normal font-[poppins] text-sm sm:text-md lg:text-lg leading-tight">
+            Industry experts in both technology and voice solutions,
+            LogicPoint is your trusted partner for setting up cutting-edge
+            customer service call centers and deploying seamless IT solutions.
+            With a wealth of experience, we bring unparalleled expertise to the
+            forefront, ensuring your business stays ahead in the dynamic
+            landscape of today's tech-driven world. At LogicPoint, we don't just
+            offer solutions; we deliver strategies that redefine excellence.
+            Elevate your operations, enhance customer interactions, and navigate
+            the digital realm with confidence. We are innovative,
+            reliabile, and a committed to your organiztion's success in every technological
+            endeavor.
+          </p>
         </div>
-        
-  
-        </div>
-     
-
-
+      </div>
+    </div>
   );
 };
 
