b6a689108f9ec313d45a04cdbb745f1aaec1cef0
Godslove Udo <udogodslove@gmail.com>
2024-01-29 18:13:27 +0100

    Update Navbar.jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Hamburger from "hamburger-react";
 import Links from "./Links";
 import NavItems from "../items/NavItems";
