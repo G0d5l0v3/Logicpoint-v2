c89b89c776485c0b2b8843b86dcecda84e0a634e
Godslove Udo <udogodslove@gmail.com>
2024-01-30 06:44:32 +0100

    update AboutPage

--- src/pages/about-us/section/AboutWhatWeDo.jsx
+++ src/pages/about-us/section/AboutWhatWeDo.jsx
@@ -1,6 +1,6 @@
 const AboutWhatWeDo = () => {
   return (
-    <div className="bg-[#181830] px-[1rem] pt-[5rem] pb-[10rem] text-white font-[exo]">
+    <div className="bg-[#181830] px-[1rem] pt-[5rem] pb-[7rem] text-white font-[exo]">
       <div className="grid md:flex items-center text-center justify-center lg:max-w-[1240px] lg:mx-[auto]">
         <div>
           <h1 className="font-semibold text-3xl sm:text-4xl lg:text-5xl ">
@@ -9,17 +9,17 @@ const AboutWhatWeDo = () => {
             </span>{" "}
             <span>We Are</span>
           </h1>
-          <p className="pt-[3rem] lg:whitespace-normal font-[poppins] text-sm sm:text-md lg:text-lg leading-tight">
-            Industry experts in both technology and voice solutions,
+          <p className="pt-[3rem] lg:whitespace-normal font-[exo] text-sm sm:text-md lg:text-lg leading-tight">
             LogicPoint is your trusted partner for setting up cutting-edge
             customer service call centers and deploying seamless IT solutions.
-            With a wealth of experience, we bring unparalleled expertise to the
+            Industry experts in both technology and voice solutions, with a
+            wealth of experience, we bring unparalleled expertise to the
             forefront, ensuring your business stays ahead in the dynamic
             landscape of today's tech-driven world. At LogicPoint, we don't just
             offer solutions; we deliver strategies that redefine excellence.
             Elevate your operations, enhance customer interactions, and navigate
-            the digital realm with confidence. We are innovative,
-            reliabile, and a committed to your organiztion's success in every technological
+            the digital realm with confidence. We are innovative, reliabile, and
+            a committed to your organiztion's success in every technological
             endeavor.
           </p>
         </div>
--- src/pages/about-us/section/WhatWeOffer.jsx
+++ src/pages/about-us/section/WhatWeOffer.jsx
@@ -1,16 +1,32 @@
 import React from "react";
+import Button from "../../../components/button/Button";
 
 const WhatWeOffer = () => {
   return (
-    <div className="relative  py-64">
-      <div className="absolute top-0 left-0 right-0 -m-8 bg-[#f2f5f8] w-[90%] md:w-[90%] lg:w-[90%] xl:w-[80%]  py-8 sm:py-12 md:py-16 lg:py-20 shadow-xl max-w-[1240px] mx-auto px-4 sm:px-6 lg:px-8">
+    <div className="relative bg-[#f2f5f8] py-[15rem] md:py-[16rem] lg:py-[18rem]">
+      <div className="absolute top-0 left-0 right-0 -m-8 bg-white w-[90%] md:w-[90%] lg:w-[90%] xl:w-[80%]  py-8 sm:py-12 md:py-16 lg:py-20 shadow-xl max-w-[1240px] mx-auto px-4 sm:px-6 lg:px-8">
         <div className="">
           <h1 className="font-[exo] font-semibold text-3xl sm:text-4xl lg:text-5xl text-[#181830]">
             <span className="border-b-8 border-[#5D1570]">What</span>
             <span className="text-[#E87B37]"> We Offer</span>{" "}
           </h1>
-
-
+          <p className="pt-[3rem] lg:whitespace-normal font-[poppins] text-sm sm:text-md lg:text-lg leading-tight">
+            We provide first-class contact center services from our ultramodern
+            facilities in Abuja, Kaduna and Lagos. Our services are tailored to
+            perfectly suit our clients’ needs and preferences, with a focus on
+            service quality, low-cost and scalable solution
+          </p>
+          <p className="pt-[1rem] lg:whitespace-normal font-[poppins] text-sm sm:text-md lg:text-lg leading-tight">
+            We help our clients become operationally efficient and competitive.
+            We have created over 1000 direct graduate jobs for customer care
+            executives, sales, marketing, tech, accounting, legal practitioners
+            and medical professionals serving the global market. Outsource
+            Global seeks to positively impact the world of business, by
+            fostering exponential growth.
+          </p>
+        </div>
+        <div className="pt-[3rem]">
+          <Button description="Get In Touch" />
         </div>
       </div>
     </div>
@@ -18,4 +34,3 @@ const WhatWeOffer = () => {
 };
 
 export default WhatWeOffer;
-
