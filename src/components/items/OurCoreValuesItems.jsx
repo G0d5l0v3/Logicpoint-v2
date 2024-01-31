b176b6504c571f2d37be75665300386e030367b5
Godslove Udo <udogodslove@gmail.com>
2024-01-31 23:27:40 +0100

    Create OurCoreValuesItems.jsx

---
+++ src/components/items/OurCoreValuesItems.jsx
@@ -0,0 +1,32 @@
+const OurCoreValuesItems = [
+  {
+    id: 1,
+    title: "Integrity",
+    description:
+      "With a strong moral compass, our colleagues and customers can expect complete honesty and transparency at all times.",
+    path: "",
+  },
+  {
+    id: 2,
+    title: "High Standards",
+    description:
+      "CRM Mastery: Elevate Relationships, Boost Efficiency with Customized Solutions.",
+    path: "",
+  },
+  {
+    id: 3,
+    title: "Measurable Impact",
+    description:
+      "We obsess over serving our customers and aim to create partnerships that expediates growth.",
+    path: "",
+  },
+  {
+    id: 4,
+    title: "Constant Improvement",
+    description:
+      "We value and encourage personal growth and we are aggressive towards meeting our goals.",
+    path: "",
+  },
+];
+
+export default OurCoreValuesItems;
