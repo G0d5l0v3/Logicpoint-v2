38d6fc0b5bc3217a7191890d1b0b3c8909d3a8ac
Godslove Udo <udogodslove@gmail.com>
2024-02-03 11:18:58 +0100

    Update WhatWeOffer.jsx

--- src/pages/about-us/sections/WhatWeOffer.jsx
+++ src/pages/about-us/sections/WhatWeOffer.jsx
@@ -2,7 +2,7 @@ import Button from "../../../components/button/Button";
 
 const WhatWeOffer = ({items}) => {
   return (
-    <div className="relative bg-[] font-[exo] py-[15rem] md:py-[16rem] lg:py-[18rem]">
+    <div className="relative bg-[#f2f5f8] font-[exo] py-[15rem] md:py-[16rem] lg:py-[18rem]">
       <div className="absolute top-0 left-0 right-0 -m-8 bg-white w-[90%] md:w-[90%] lg:w-[90%] xl:w-[80%]  py-8 sm:py-12 md:py-16 lg:py-20 shadow-xl max-w-[1240px] mx-auto px-4 sm:px-6 lg:px-8">
         <div className="">
           <h1 className="font-[exo] font-semibold text-3xl sm:text-4xl lg:text-5xl text-[#181830]">
@@ -21,7 +21,7 @@ const WhatWeOffer = ({items}) => {
           </div>
         </div>
         <div className="pt-[3rem]">
-          <Button description="Get In Touch" />
+          <Button description="Talk to Us" />
         </div>
       </div>
     </div>
