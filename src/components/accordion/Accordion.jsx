be112447ffdfb4ca87b4c96124f271bf88ff9080
Godslove Udo <udogodslove@gmail.com>
2024-02-05 12:23:08 +0100

    Update Accordion.jsx

--- src/components/accordion/Accordion.jsx
+++ src/components/accordion/Accordion.jsx
@@ -9,12 +9,14 @@ const Accordion = ({ items }) => {
     <div className="grid lg:w-[50%] gap-8 ">
       {items.map((item) => (
         <div
-          className={`flex flex-col py-[1.5rem] px-[1.5rem] lg:px-[3rem] rounded-lg shadow-lg bg-white hover:cursor-pointer hover:shadow-[#cdb8d4] ${ item.id === activeItemId ? "shadow-[#cdb8d4]": ""} hover:shadow-md`}
+          className={`flex flex-col py-[1.5rem] px-[1.5rem] lg:px-[3rem] rounded-lg shadow-lg bg-white hover:cursor-pointer hover:shadow-[#cdb8d4] ${
+            item.id === activeItemId ? "hover:shadow-lg border-2 border-t-[#cdb8d4]" : ""
+          } hover:shadow-md`}
           key={item.id}
           onClick={() => handleToggle(item.id)}
         >
           <div className="flex justify-between items-center w-full">
-            <h1 className="font-semibold font-[exo]">{item.title}</h1>
+            <h1 className={`font-semibold font-[exo] ${item.id === activeItemId ? "text-[#5D1570]" : "text-black"}`}>{item.title}</h1>
             <span className="flex items-center font-bold text-white text-xl justify-center rounded-full h-10 w-10 bg-[#5D1570]">
               {item.id === activeItemId ? "-" : "+"}
             </span>
