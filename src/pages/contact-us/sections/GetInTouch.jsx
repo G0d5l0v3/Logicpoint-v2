01ddf461355e27502f5204bdca7b4c5720d43a2d
Godslove Udo <udogodslove@gmail.com>
2024-02-09 20:57:54 +0100

    Update GetInTouch.jsx

--- src/pages/contact-us/sections/GetInTouch.jsx
+++ src/pages/contact-us/sections/GetInTouch.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const GetInTouch = () => {
   return (
-    <div className="grid place-content-center px-[1rem] pt-[5rem] lg:max-w-[1240px] lg:mx-[auto] ">
+    <div className="flex flex-col justify-center px-[1rem] py-[5rem] lg:max-w-[1240px] lg:mx-[auto] ">
       <h1 className="text-center font-[exo] text-[#181830] text-3xl sm:text-4xl lg:whitespace-normal lg:text-5xl font-semibold leading-tight">
         Get In <span className="text-[#5D1570]">Touch </span>{" "}
       </h1>
@@ -10,18 +10,18 @@ const GetInTouch = () => {
         We would love to hear from you.
       </p>
 
-      <div className="grid grid-row-3">
-        <div className=" border-r px-2">
-          <h1 className="font-bold">General Information</h1>
-          <p></p>
+      <div className="grid lg:grid-cols-3 font-[exo]">
+        <div className=" lg:border-r px-[3rem]">
+          <h1 className="font-bold text-[#5D1570]">General Information</h1>
+          <p className="pt-[1.5rem]">For general information or information about the Logicpoint story, contact:</p>
         </div>
-        <div className="border-r px-2">
-          <h1 className="font-bold">Career</h1>
-          <p></p>
+        <div className="lg:border-r px-[3rem]">
+          <h1 className="font-bold text-[#5D1570]">Careers</h1>
+          <p className="pt-[1.5rem]">Are you interested in joining the Logicpoint team? Reach us at</p>
         </div>
-        <div className=" border-r px-2">
-          <h1 className="font-bold">Sales</h1>
-          <p></p>
+        <div className="px-[3rem]">
+          <h1 className="font-bold text-[#5D1570]">Sales</h1>
+          <p className="pt-[1.5rem]">To speak to us about sales and partnership ideas, say hello:</p>
         </div>
       </div>
     </div>
