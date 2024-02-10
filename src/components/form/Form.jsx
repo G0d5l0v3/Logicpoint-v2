fc204d1df7860b9dea8d4637a8f81acc36ec70ab
Godslove Udo <udogodslove@gmail.com>
2024-02-10 15:35:25 +0100

    Update Form.jsx

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -3,38 +3,38 @@ import Button from "../../components/button/Button"
 
 const Form = () => {
   return (
-    <div className="lg:max-w-[1240px] lg:mx-[auto] rounded-lg shadow-xl bg-[#181830] text-white px-[2rem] lg:px-[5rem] py-[3rem] mb-[5rem] font-[exo]">
+    <div className="lg:max-w-[1240px] lg:mx-[auto] rounded-lg shadow-xl bg-[#181830] px-[2rem] lg:px-[5rem] py-[3rem] mb-[5rem] font-[exo]">
       <form>
-        <h1 className="text-sm md:text-lg lg:text-xl font-semibold">
+        <h1 className="text-sm md:text-lg lg:text-xl font-semibold text-white">
           Or fill out the form below and send
         </h1>
         <div className="grid lg:grid lg:grid-cols-2 lg:gap-8 text-[0.8rem]">
           <div>
             <div className="grid gap-4 pt-[2rem]">
-              <label name="name">Name</label>
+              <label name="name" className="text-white">Name</label>
               <input
                 type="text"
                 name="name"
                 placeholder="Enter your name"
-                className="rounded px-[2rem] py-[1rem] text-black"
+                className="rounded px-[2rem] py-[1rem] text-black focus:outline-none"
               />
             </div>
             <div className="grid gap-4 pt-[2rem]">
-              <label name="email">Email Address</label>
+              <label name="email" className="text-white">Email Address</label>
               <input
                 type="email"
                 name="email"
                 placeholder="Enter your email address"
-                className="rounded px-[2rem] py-[1rem] text-black"
+                className="rounded px-[2rem] py-[1rem] text-black focus:outline-none"
               />
             </div>
             <div className="grid gap-4 pt-[2rem]">
-              <label name="subject">Enter Subject</label>
+              <label name="subject" className="text-white">Enter Subject</label>
               <input
                 type="text"
                 name="subject"
                 placeholder="What is the subject of this message"
-                className="rounded px-[2rem] py-[1rem] text-black"
+                className="rounded px-[2rem] py-[1rem] text-black focus:outline-none"
               />
             </div>
             <div className="hidden lg:flex lg:pt-[3rem]">
@@ -43,11 +43,11 @@ const Form = () => {
           </div>
 
           <div className="flex flex-col gap-4 pt-[2rem]">
-            <label name="msg">Enter Message</label>
+            <label name="msg" className="text-white">Enter Message</label>
             <textarea
               name="msg"
               placeholder="Write your message"
-              className="rounded px-[2rem] py-[1rem] text-black h-[15rem]"
+              className="rounded px-[2rem] py-[1rem] text-black h-[15rem] focus:outline-none"
             />
           </div>
           <div className="pt-[3rem] lg:hidden">
